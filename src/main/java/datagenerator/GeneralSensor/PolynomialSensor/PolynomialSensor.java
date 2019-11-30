3e2280daf5b16a7023b0ee14203cf8ef41fa6f7d
UnGuZartex <cobra0415@hotmail.com>
2019-11-30 16:42:11 +0100

    Polynomial is done

--- src/main/java/datagenerator/GeneralSensor/PolynomialSensor/PolynomialSensor.java
+++ src/main/java/datagenerator/GeneralSensor/PolynomialSensor/PolynomialSensor.java
@@ -16,11 +16,18 @@ public class PolynomialSensor extends Sensor {
 
     public PolynomialSensor(int radius, List<Pair>points) {
         super(radius);
-        pointsKnown.add(new Pair<Double,Double>(0.0,1.0));
-        pointsKnown.add(new Pair<Double,Double>(1.0,0.0));
+        pointsKnown.add(new Pair<Double,Double>(1.0,6.0));
+        pointsKnown.add(new Pair<Double,Double>(2.0,9.0));
+        pointsKnown.add(new Pair<Double,Double>(3.0,2.0));
+        pointsKnown.add(new Pair<Double,Double>(4.0,5.0));
+
+
         calculateNewtonCoefficients();
         System.out.println(newtonCoefficients);
+
         timeUnit = TimeUnit.SECONDS;
+        System.out.println(evaluatePolynomial(LocalTime.of(0,0,4)));
+
     }
 
     @Override
@@ -33,32 +40,35 @@ public class PolynomialSensor extends Sensor {
 
 
     private void calculateNewtonCoefficients() {
-        Map<String,Map<String,Double>> Coefficients = new HashMap<>();
+        Map<Integer,Map<Integer,Double>> Coefficients = new HashMap<>();
         int order = pointsKnown.size() - 1;
-        Map<String,Double> orderZero = new HashMap<>();
+        Map<Integer,Double> orderZero = new HashMap<>();
         int calculatingOrder = 0;
         List<Double> Xlist = new ArrayList<>();
 
         for (int i = 0; i < pointsKnown.size(); i++){
-            orderZero.put(String.valueOf(i), pointsKnown.get(i).getRight());
+            orderZero.put(i, pointsKnown.get(i).getRight());
             Xlist.add(pointsKnown.get(i).getLeft());
         }
-        Coefficients.put(String.valueOf(calculatingOrder), orderZero);
+        Coefficients.put(calculatingOrder, orderZero);
         while(calculatingOrder < order){
             calculatingOrder++;
-            Coefficients.put(String.valueOf(calculatingOrder), getCoeffWithOrder(Coefficients,Xlist));
+            Coefficients.put(calculatingOrder, getCoeffWithOrder(Coefficients,Xlist, calculatingOrder));
         }
         System.out.println(Coefficients);
+        for(int i = 0; i < Coefficients.size(); i++){
+            newtonCoefficients.add(Coefficients.get(i).get(0));
+        }
     }
 
-    private Map<String, Double> getCoeffWithOrder(Map<String, Map<String,Double>> alreadyKnown,
-                                                  List<Double> X){
-        Map<String, Double> previousOrder = alreadyKnown.get(String.valueOf(alreadyKnown.size()-1));
-        Map<String, Double> results = new HashMap<>();
+    private Map<Integer, Double> getCoeffWithOrder(Map<Integer, Map<Integer,Double>> alreadyKnown,
+                                                  List<Double> X, int order){
+        Map<Integer, Double> previousOrder = alreadyKnown.get(alreadyKnown.size()-1);
+        Map<Integer, Double> results = new HashMap<>();
         for(int i = 0; i < previousOrder.size()-1; i++){
-            Double currentValue = previousOrder.get(String.valueOf(i));
-            Double nextValue = previousOrder.get(String.valueOf(i+1));
-            results.put(String.valueOf(i),((nextValue-currentValue)/(X.get(i + 1) - X.get(i))));
+            Double currentValue = previousOrder.get(i);
+            Double nextValue = previousOrder.get(i+1);
+            results.put(i,((nextValue-currentValue)/(X.get(i+order) - X.get(i))));
         }
 
         return results;
@@ -68,7 +78,16 @@ public class PolynomialSensor extends Sensor {
         double totalValue = 0;
         for (int i = 0; i < newtonCoefficients.size(); i++)
         {
-            totalValue += newtonCoefficients.get(i) * Math.pow(timeToEvaluate, i);
+            totalValue += newtonCoefficients.get(i) * getPointsFromOrder(i, timeToEvaluate);
+            System.out.println(i + ": " + totalValue);
+        }
+        return totalValue;
+    }
+
+    private Double getPointsFromOrder(int order, double timeToEvaluate) {
+        Double totalValue = pointsKnown.get(0).getLeft();
+        for(int i = 0; i < order; i++){
+            totalValue *= timeToEvaluate-pointsKnown.get(i).getLeft();
         }
         return totalValue;
     }
